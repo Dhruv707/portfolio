a4413c8393be1611c037fcc49e1b965c8beab72f
Dhruv707 <khatri.dhruv07@gmail.com>
2024-03-07 07:35:14 +0530

    first commit

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div className="navbar">
+      <h3>Dhruv.dev</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Navbar;
